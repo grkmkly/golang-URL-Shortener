a6da1da130bfbae3279effe1fdb7b6f3ec17dc37
grkmkly <kolaygorkem@icloud.com>
2025-01-25 00:30:01 +0300

    fixed problems for server

--- api/main.go
+++ api/main.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
+	"github.com/rs/cors"
 )
 
 var myModel *model.URLModel = new(model.URLModel)
@@ -19,8 +20,14 @@ func main() {
 	godotenv.Load(".env")
 	port = os.Getenv("PORT")
 	r := mux.NewRouter()
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"}, // Burada tüm domainlere izin veriyoruz, belirli bir domain yazabilirsiniz.
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"Content-Type", "Authorization"},
+	})
+	handlerWithCORS := c.Handler(r)
 	myModel.Port = port
 	handler.MainHandler(r, myModel)
 	fmt.Println("Server is running on port: " + myModel.Port)
-	http.ListenAndServe(":"+myModel.Port, r)
+	http.ListenAndServe(":"+myModel.Port, handlerWithCORS)
 }
--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module api/main.go
 
 go 1.23.1
 
-require github.com/gorilla/mux v1.8.1
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/rs/cors v1.11.1
+)
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require github.com/joho/godotenv v1.5.1
